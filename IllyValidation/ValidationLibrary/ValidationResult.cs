f856ffd8288cde163cc958c880453f0331b8dbf2
Kojo6771 <230165858@aston.ac.uk>
2026-09-04 15:52:11 +0100

    Update ValidationResult.cs
    Added comments to represent what the records are meant for

--- IllyValidation/ValidationLibrary/ValidationResult.cs
+++ IllyValidation/ValidationLibrary/ValidationResult.cs
@@ -1,7 +1,10 @@
 namespace ValidationLibrary;
 
-
+// Represents the result of a validation operation, which can be either a success or an error.
 public abstract record ValidationResult<TSuccess, TError>;
 
+// Represents a successful validation result.
 public sealed record Success<TSuccess, TError>(TSuccess Value) : ValidationResult<TSuccess, TError>;
+
+// Represents a failed validation result.
 public sealed record Error<TSuccess, TError>(TError Value) : ValidationResult<TSuccess, TError>;
