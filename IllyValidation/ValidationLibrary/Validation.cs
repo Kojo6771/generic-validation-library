ca15de65efb6e65e15149e847c725c34b9f4e30f
Kojo6771 <230165858@aston.ac.uk>
2026-09-04 16:09:32 +0100

    Add Validation success factory
    Added a generic Success factory method

--- IllyValidation/ValidationLibrary/Validation.cs
+++ IllyValidation/ValidationLibrary/Validation.cs
@@ -3,4 +3,12 @@ namespace ValidationLibrary;
 public static class Validation
 {
     
+    // Validates a value and returns a ValidationResult indicating success or error.
+    public static ValidationResult<TSuccess, TError> Success<TSuccess, TError>(TSuccess value)
+    {
+        return new Success<TSuccess, TError>(value);
+    }
+
+
+    
 }
