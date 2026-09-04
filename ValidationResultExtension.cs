7a1517d01f0d9e2f1a34c5c738af38b887bcdefe
Kojo6771 <230165858@aston.ac.uk>
2026-09-04 23:27:20 +0100

    Added a Map extension for ValidationResult
    I've added a generic Map extension method to transform successful values while preserving validation errors unchanged.

--- ValidationResultExtension.cs
+++ ValidationResultExtension.cs
@@ -2,5 +2,20 @@ namespace ValidationLibrary;
 
 public static class ValidationResultExtension
 {
-    
+    // Maps the success value of a ValidationResult to a new value, preserving errors.
+    public static ValidationResult<TResult, TError> Map<TSuccess, TResult,TError>(
+        this ValidationResult<TSuccess, TError> result,
+        Func<TSuccess, TResult> map)
+    {
+        return result switch
+        {
+          Success<TSuccess, TError> s => new Success<TResult, TError>(map(s.value)),
+          Error<TSuccess, TError> e => new Error<TResult, TError>(e.errors),
+
+          _ => throw new InvalidOperationException("Unknown ValidationResult")
+        };
+    }
+
+
+
 }
