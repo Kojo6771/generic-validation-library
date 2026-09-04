eb17d4966457fabdf51cb35ef1c517c7a9d29e27
Kojo6771 <230165858@aston.ac.uk>
2026-09-04 20:11:48 +0100

    Update Validation.cs
    Update the validation factory so `Error` accepts multiple errors instead of a single value.

--- IllyValidation/ValidationLibrary/Validation.cs
+++ IllyValidation/ValidationLibrary/Validation.cs
@@ -9,10 +9,28 @@ public static class Validation
         return new Success<TSuccess, TError>(value);
     }
 
-    public static ValidationResult<TSuccess, TError> Error<TSuccess, TError>(TError value)
+    public static ValidationResult<TSuccess, TError> Error<TSuccess, TError>(TError[] errors)
     {
-        return new Error<TSuccess, TError>(value);
+        return new Error<TSuccess, TError>(errors);
     }
     
-    
+    public static ValidationResult<TResult, TError> Combine<T1, T2, TResult, TError>(
+        ValidationResult<T1, TError> result1,
+        ValidationResult<T2, TError> result2,
+        Func<T1, T2, TResult> createResult)
+    {
+        var error = new List<TError>();
+
+        if (result1 is Error<T1, TError> error1)
+        {
+            error.AddRange(error1.errors);
+        }
+
+        if (result2 is Error<T2, TError> error2)
+        {
+            error.AddRange(error2.errors);
+        }
+
+        
+    }
 }
