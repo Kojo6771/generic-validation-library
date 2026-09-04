680d78c3b84a8f3d54863c8b6ee9bb53219954e7
Kojo6771 <230165858@aston.ac.uk>
2026-09-04 22:51:29 +0100

    Complete and extend Validation
    Finished the 2 result and 3 result Combine implementations by returning aggregated errors or a success value built from the validated inputs.

--- IllyValidation/ValidationLibrary/Validation.cs
+++ IllyValidation/ValidationLibrary/Validation.cs
@@ -19,18 +19,63 @@ public static class Validation
         ValidationResult<T2, TError> result2,
         Func<T1, T2, TResult> createResult)
     {
-        var error = new List<TError>();
+        var errors = new List<TError>();
 
         if (result1 is Error<T1, TError> error1)
         {
-            error.AddRange(error1.errors);
+            errors.AddRange(error1.errors);
         }
 
         if (result2 is Error<T2, TError> error2)
         {
-            error.AddRange(error2.errors);
+            errors.AddRange(error2.errors);
         }
 
-        
+        if(errors.Count > 0)
+        {
+            return new Error<TResult, TError>(errors);
+        }
+
+        var firstValue = ((Success<T1, TError>)result1).value;
+        var secondValue = ((Success<T2, TError>)result2).value;
+
+        return new Success<TResult, TError>(createResult(firstValue, secondValue));
+
     }
+
+    public static ValidationResult<TResult, TError> Combine<T1, T2, T3, TResult, TError>(
+        ValidationResult<T1, TError> result1,
+        ValidationResult<T2, TError> result2,
+        ValidationResult<T3, TError> result3,
+        Func<T1, T2, T3, TResult> createResult)
+    {
+        var errors = new List<TError>();
+
+        if (result1 is Error<T1, TError> error1)
+        {
+            errors.AddRange(error1.errors);
+        }
+
+        if (result2 is Error<T2, TError> error2)
+        {
+            errors.AddRange(error2.errors);
+        }
+
+        if(result3 is Error<T3, TError> error3)
+        {
+            errors.AddRange(error3.errors);
+        }
+
+        if(errors.Count > 0)
+        {
+            return new Error<TResult, TError>(errors);
+        }
+
+        var firstValue = ((Success<T1, TError>)result1).value;
+        var secondValue = ((Success<T2, TError>)result2).value;
+        var thirdValue = ((Success<T3, TError>)result3).value;
+
+        return new Success<TResult, TError>(createResult(firstValue, secondValue, thirdValue));
+    }
+
 }
