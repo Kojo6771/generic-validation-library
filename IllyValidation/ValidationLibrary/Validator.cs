348f5becb2befcba848e0503b484f9b675b6de97
Kojo6771 <230165858@aston.ac.uk>
2026-09-05 01:20:10 +0100

    Add generic Validator with map support
    Introduced a new `Validator" type in `ValidationLibrary` that wraps validation logic as a function

---
+++ IllyValidation/ValidationLibrary/Validator.cs
@@ -0,0 +1,32 @@
+namespace ValidationLibrary;
+
+public sealed class Validator<TInput, TOutput, TError>
+{
+    // The function that performs the validation
+    private readonly Func<TInput, ValidationResult<TOutput, TError>> _validate;
+
+    // Validator constructor
+    public Validator(
+        Func<TInput, ValidationResult<TOutput, TError>> validate
+    )
+    {
+        _validate = validate;
+    }
+
+
+    public ValidationResult<TOutput, TError> Validate(TInput input)
+    {
+        return _validate(input);
+    }
+
+    // Maps the output of the validator to a new output type, preserving errors.
+    public Validator<TInput, TNewOutput, TError> Map<TNewOutput>(Func<TOutput, TNewOutput> map)
+    {
+        return new Validator<TInput, TNewOutput, TError>(
+            input => _validate(input).Map(map)
+        );
+    }
+
+    
+
+}
