ce668981ffcb87cbf578fbfde76a88f86e1f47e1
Kojo6771 <230165858@aston.ac.uk>
2026-09-05 14:12:31 +0100

    created required text
    created a method which validates if the given string is not null, empty or whitespace

--- IllyValidation/ValidationLibrary/Rules.cs
+++ IllyValidation/ValidationLibrary/Rules.cs
@@ -3,5 +3,13 @@ namespace ValidationLibrary;
 // A static class containing common validation rules.
 public static class Rules
 {
-    
+    // Validates that the given string is not null, empty, or whitespace.
+    public static ValidationResult<string, TError> RequiredText<TError>(string? value, TError error)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return Validation.Failure<string, TError>(error);
+        }
+        return Validation.Success<string, TError>(value!);
+    }
 }
