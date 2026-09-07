0d7128051def9e22acfed69b7384d78ac784202c
Kojo6771 <230165858@aston.ac.uk>
2026-09-07 03:29:38 +0100

    Update PersonValidator.cs
    created the class and added fields

--- IllyValidation/ValidationDemo/PersonValidator.cs
+++ IllyValidation/ValidationDemo/PersonValidator.cs
@@ -1,3 +1,14 @@
 using ValidationLibrary;
 
 namespace ValidationDemo;
+
+public static class PersonValidator
+{
+    private const int MaximumNameLength = 100;
+
+    private static readonly DateTime MinimumDOB =
+        new DateTime(1905, 1, 1);
+
+    public static Validator<PersonInput, ValidPerson, PersonValidation> Create(
+        DateTime? today = null){}
+}
